ab3db964a4dc39d05161726b902f4ed81679f6b6
viplavkumar96 <viplav.kv98@gmail.com>
2018-11-01 13:27:37 +0530

    Add database support

--- security.py
+++ security.py
@@ -1,16 +1,16 @@
 
+from models import User
 #File that contains functions for authenticate users
 #from models import User
 from werkzeug.security import safe_str_cmp
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
 def authenticate(user,password): #method for authenticating users
     data = request.get_json()
-    username = data['user']
+    usernm = data['user']
     pwd = data['passwd'] 
-    if safe_str_cmp(username,user) and safe_str_cmp(pwd,password):
+    user_data = User.query.filter_by(username = usernm,password = pwd)
+    if user_data is not None:
         return user
 
 def identity(payload): #To return the JWT token when user is successfully logged in
