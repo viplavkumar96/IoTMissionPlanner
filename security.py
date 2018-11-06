ee9e4b3407e1abfa30043a93c3d1d0e65d11300f
viplavkumar96 <viplav.kv98@gmail.com>
2018-11-06 09:57:27 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,18 +1,22 @@
-
-from models import User
 #File that contains functions for authenticate users
 #from models import User
 from werkzeug.security import safe_str_cmp
 from flask_sqlalchemy import SQLAlchemy
+from models import User
+from flask import flash,render_template
 
-def authenticate(user,password): #method for authenticating users
-    data = request.get_json()
-    usernm = data['user']
-    pwd = data['passwd'] 
-    user_data = User.query.filter_by(username = usernm,password = pwd) or None
-    if user_data is not None:
-        return user
+
+def authenticate(user,pwd): #method for authenticating users
+    if user is not None and pwd is not None:
+        user_data = User.query.filter_by(username = user,password = pwd) or None
+
+        if user_data is not None:
+                return user
+        else:
+                flash("User not logged in")
+                return render_template("login.html")
 
 def identity(payload): #To return the JWT token when user is successfully logged in
     user_id = payload['identity']
-    return 
+    return user_id
+
