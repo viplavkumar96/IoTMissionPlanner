db760aa46ec2b9fc23b326c35406be4369abbc14
viplavkumar96 <viplav.kv98@gmail.com>
2018-11-06 09:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask,jsonify,request,render_template
 from flask_restful import Resource,Api
-from flask_jwt import JWT
+from flask_jwt import JWT,jwt_required
 from security import authenticate , identity
+from models import Mission,User
+
 
 app = Flask(__name__)
 app.secret_key = "wgetterminalnow"
 
 api = Api(app)
-jwt = JWT(app,authenticate,identity) #new endpoint /auth for authorization
-#@jwt_required decorator for JWT authentication
+jwt = JWT(app,authenticate,identity) #new endpoint /auth for authorization4
 
-class Mission(Resource):
-    #@jwt_required
-    def post(self):
-        return render_template('mission.html') #Loading the mission form
+@app.route('/mission', methods = ['GET', 'POST'])
+@jwt_required
+def mission():
+    return render_template('mission.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/',methods = ['GET','POST'])
+def home():
+    return render_template('login.html')
     
 
-api.add_resource(Mission,'/mission/')
+
 
 app.run()
 
